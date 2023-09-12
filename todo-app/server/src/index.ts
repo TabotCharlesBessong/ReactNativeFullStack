d91860ec4ac7e050015795d54474d8e102be5ba5
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-12 17:51:40 +0100

    basic post request

--- todo-app/server/src/index.ts
+++ todo-app/server/src/index.ts
@@ -4,9 +4,14 @@ import express from 'express'
 // create a server
 const app = express()
 
-app.get('/',(req,res) => {
-  res.send('<h1>Hello world, how are you!</h1>')
-})
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.post("/", (req, res) => {
+  // res.send('<h1>Hello world, how are you!</h1>')
+  res.json({ message: "I am listening to you" });
+  console.log(req.body);
+});
 
 // listen to some port
 app.listen(3000,() => {
