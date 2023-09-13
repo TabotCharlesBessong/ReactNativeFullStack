8c31e9c7b659241e9009843ebd42636c3b6a7afa
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-13 12:12:32 +0100

    handling data types with interface

--- todo-app/server/src/index.ts
+++ todo-app/server/src/index.ts
@@ -1,27 +1,32 @@
 import express from "express";
-import './db'
+import "./db";
 import note from "./model/note";
 
 // create a server
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.post(
-  "/",
-  (req, res) => {
-    // res.send('<h1>Hello world, how are you!</h1>')
-    res.json({ message: "I am listening to you" });
-    console.log(req.body);
-  }
-);
+interface IncomingBody {
+  title: string;
+  description?: string;
+}
 
-app.post("/create",async (req,res) => {
-  const newNote = new note({title:req.body.title,description:req.body.description})
-  await newNote.save()
-  res.json({message:"I am listening to create!"})
-})
+app.post("/", (req, res) => {
+  // res.send('<h1>Hello world, how are you!</h1>')
+  res.json({ message: "I am listening to you" });
+  console.log(req.body);
+});
+
+app.post("/create", async (req, res) => {
+  const newNote = new note({
+    title: req.body.title as IncomingBody,
+    description: req.body.description as IncomingBody,
+  });
+  await newNote.save();
+  res.json({ message: "I am listening to create!" });
+});
 
 // listen to some port
 app.listen(3000, () => {
