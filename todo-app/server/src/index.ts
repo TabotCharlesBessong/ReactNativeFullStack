a85e8ec97c208201e63a428ffbfa58fd5e4ca894
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-13 11:15:27 +0100

    middlewares

--- todo-app/server/src/index.ts
+++ todo-app/server/src/index.ts
@@ -3,25 +3,22 @@ import express from "express";
 // create a server
 const app = express();
 
-// app.use(express.json())
-// app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.post(
   "/",
-  (req, res, next) => {
-    // manipulate things here
-    // read the data and want to add it to request.body
-    req.on("data",(chunk) => {
-      req.body = JSON.parse(chunk)
-      next()
-    })
-  },
   (req, res) => {
     // res.send('<h1>Hello world, how are you!</h1>')
     res.json({ message: "I am listening to you" });
     console.log(req.body);
   }
 );
+
+app.post("/create",(req,res) => {
+  console.log(req.body)
+  res.json({message:"I am listening to create!"})
+})
 
 // listen to some port
 app.listen(3000, () => {
