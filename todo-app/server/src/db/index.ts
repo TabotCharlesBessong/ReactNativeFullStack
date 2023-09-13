4c87003b73e56ff60fc052d12cfe1197f4b29df7
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-13 13:06:15 +0100

    fix:(fixed some the uri connection string problem)

--- todo-app/server/src/db/index.ts
+++ todo-app/server/src/db/index.ts
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
 dotenv.config();
 
+const mongoURI = process.env.MONGO_URI;
+
+if (!mongoURI) {
+  console.error("MONGO_URI is not defined in the environment variables.");
+  process.exit(1);
+}
+
 mongoose
-  .connect(
-    `process.env.MONGO_URI`
-  )
+  .connect(mongoURI)
   .then(() => {
-    console.log("connected to db");
+    console.log("Connected to the database");
   })
   .catch((err) => {
-    console.log(err);
+    console.error("Failed to connect to the database:", err);
   });
