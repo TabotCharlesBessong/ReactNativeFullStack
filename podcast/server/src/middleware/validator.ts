36899c1a4d72c070d00a9fa3e18f18e334a6f1b7
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-15 12:27:24 +0100

    validation

---
+++ podcast/server/src/middleware/validator.ts
@@ -0,0 +1,29 @@
+
+import { RequestHandler } from "express";
+import * as yup from "yup";
+
+export const validate = (schema: any): RequestHandler => {
+  return async (req, res, next) => {
+    if (!req.body) return res.json({ error: "Empty body is not excepted!" });
+    const schemaToValidate = yup.object({
+      body: schema,
+    });
+
+    try {
+      await schemaToValidate.validate(
+        {
+          body: req.body,
+        },
+        {
+          abortEarly: true,
+        }
+      );
+
+      next();
+    } catch (error) {
+      if (error instanceof yup.ValidationError) {
+        res.json({ error: error.message });
+      }
+    }
+  };
+};
--- podcast/server/src/routes/auth.ts
+++ podcast/server/src/routes/auth.ts
@@ -1,14 +1,19 @@
-import { CreateUser } from './../@types/user';
-import User from '../models/users'
+import { validate } from "./../middleware/validator";
+import { CreateUserSchema } from "./../utils/validationSchema";
+import { CreateUser } from "./../@types/user";
+import User from "../models/users";
 import { Router } from "express";
 
-const router = Router() 
+const router = Router();
 
-router.post('/create',(req:CreateUser,res) => {
-  const {email,name,password} = req.body
-  const user = new User({email,name,password})
-  user.save()
-  res.json({user})
-})
+router.post("/create", validate(CreateUserSchema), async(req: CreateUser, res) => {
+  const { email, name, password } = req.body;
+  // CreateUserSchema.validate({email,name,password}).catch(err => {
+  //   console.log(err)
+  // })
+  const user = new User({ email, name, password });
+  user.save();
+  res.json({ user });
+});
 
-export default router
+export default router;
