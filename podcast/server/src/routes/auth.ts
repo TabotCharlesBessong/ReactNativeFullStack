b98c650c0bec7014a25de8d48c3c4bcb64f15541
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-15 10:26:46 +0100

    fixing type issues

--- podcast/server/src/routes/auth.ts
+++ podcast/server/src/routes/auth.ts
@@ -1,10 +1,10 @@
-
+import { CreateUser } from './../@types/user';
 import User from '../models/users'
 import { Router } from "express";
 
 const router = Router() 
 
-router.post('/create',(req,res) => {
+router.post('/create',(req:CreateUser,res) => {
   const {email,name,password} = req.body
   const user = new User({email,name,password})
   user.save()
