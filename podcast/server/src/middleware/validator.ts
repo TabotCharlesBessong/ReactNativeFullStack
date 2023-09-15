91b4ec8d610f64f60aa57d2f35d467321694e39e
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-15 12:37:32 +0100

    422 error

--- podcast/server/src/middleware/validator.ts
+++ podcast/server/src/middleware/validator.ts
@@ -22,7 +22,7 @@ export const validate = (schema: any): RequestHandler => {
       next();
     } catch (error) {
       if (error instanceof yup.ValidationError) {
-        res.json({ error: error.message });
+        res.status(422).json({ error: error.message });
       }
     }
   };
