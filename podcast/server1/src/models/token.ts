e4c8a86a0ccd69fb2c0af4b42c4d04e4d05bad1b
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-16 15:19:45 +0100

    created token schema

---
+++ podcast/server1/src/models/token.ts
@@ -0,0 +1,30 @@
+
+import { Model, model, ObjectId, Schema } from "mongoose";
+
+// interface (typescript)
+interface TokenDocument {
+  owner: ObjectId
+  token:string
+  createdAt:Date
+}
+
+const tokenSchema = new Schema<TokenDocument>(
+  {
+    owner:{
+      type:Schema.Types.ObjectId,
+      required:true,
+      ref:"User"
+    },
+    token:{
+      type:String,
+      required:true
+    },
+    createdAt:{
+      type:Date,
+      expires:3600,
+      default:Date.now()
+    }
+  }
+);
+
+export default model("User", tokenSchema) as Model<TokenDocument>;
