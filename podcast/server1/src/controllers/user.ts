576d681794259efc09fe54173f5135cec3f60fda
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-16 15:09:44 +0100

    sending email

--- podcast/server1/src/controllers/user.ts
+++ podcast/server1/src/controllers/user.ts
@@ -1,11 +1,29 @@
 import { RequestHandler } from "express";
+import nodemailer from 'nodemailer'
 
 import { CreateUser } from "#/@types/user";
 import User from "#/models/user";
+import { MAILTRAP_PASS, MAILTRAP_USER } from "#/utils/variables";
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { email, password, name } = req.body;
 
   const user = await User.create({ name, email, password });
+
+  // send verifica email
+  const transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: MAILTRAP_USER,
+      pass: MAILTRAP_PASS,
+    },
+  });
+
+  transport.sendMail({
+    to:user.email,
+    from:"ebezebeatrice@gmail.com",
+    html:"<h1>12345678</h1>"
+  })
   res.status(201).json({ user });
 };
--- podcast/server1/src/utils/variables.ts
+++ podcast/server1/src/utils/variables.ts
@@ -1,3 +1,5 @@
 const { env } = process as { env: { [key: string]: string } };
 
-export const MONGO_URI = env.MONGO_URI;
+export const MONGO_URI = env.MONGO_URI
+export const MAILTRAP_USER = env.MAILTRAP_USER
+export const MAILTRAP_PASS = env.MAILTRAP_PASS
