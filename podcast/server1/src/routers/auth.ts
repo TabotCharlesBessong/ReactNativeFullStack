e2763820ee887c23578012324c6e10fde308ee10
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-18 07:08:52 +0100

    refactoring to middleware

--- podcast/server1/src/routers/auth.ts
+++ podcast/server1/src/routers/auth.ts
@@ -1,10 +1,11 @@
 import {
   create,
   generateForgetPasswordLink,
-  isValidPassResetToken,
+  grantValid,
   sendReVerificationToken,
   verifyEmail,
 } from "#/controllers/user";
+import { isValidPassResetToken } from "#/middleware/auth";
 import { validate } from "#/middleware/validator";
 import {
   CreateUserSchema,
@@ -18,6 +19,11 @@ router.post("/create", validate(CreateUserSchema), create);
 router.post("/verify-email", validate(TokenAndIDValidation), verifyEmail);
 router.post("/re-verify-email", sendReVerificationToken);
 router.post("/forget-password", generateForgetPasswordLink);
-router.post("/verify-pass-reset-token",validate(TokenAndIDValidation),isValidPassResetToken)
+router.post(
+  "/verify-pass-reset-token",
+  validate(TokenAndIDValidation),
+  isValidPassResetToken,
+  grantValid
+);
 
 export default router;
