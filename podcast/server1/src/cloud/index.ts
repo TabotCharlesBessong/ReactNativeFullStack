06adc19f749be1f6b3758b89f0d51093841cefc1
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-22 20:49:42 +0100

    facing some chalenges

--- podcast/server1/src/cloud/index.ts
+++ podcast/server1/src/cloud/index.ts
@@ -1,12 +1,11 @@
-
-import { CLOUD_KEY, CLOUD_NAME, CLOUD_SECRET } from "#/utils/variables"
-import {v2 as cloudinary} from "cloudinary"
+import { CLOUD_KEY, CLOUD_NAME, CLOUD_SECRET } from "#/utils/variables";
+import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name:CLOUD_NAME,
-  api_key:CLOUD_KEY,
-  api_secret:CLOUD_SECRET,
-  secure:true
-})
+  cloud_name: CLOUD_NAME,
+  api_key: CLOUD_KEY,
+  api_secret: CLOUD_SECRET,
+  secure: true,
+});
 
-export default cloudinary
+export default cloudinary;
--- podcast/server1/src/middleware/fileParser.ts
+++ podcast/server1/src/middleware/fileParser.ts
@@ -1,25 +1,24 @@
-import { Request } from "express";
-import { RequestHandler } from "express-serve-static-core";
-import formidable,{Files} from "formidable";
+import { Request, RequestHandler } from "express";
+import formidable, { Files } from "formidable";
 
 export interface RequestWithFiles extends Request {
-  files?:Files
+  files?: Files;
 }
 
-const fileParser:RequestHandler = (req:RequestWithFiles,res,next) => {
-  if (!req.headers["content-type"]?.startsWith("multipart/form-data"))
+const fileParser: RequestHandler = (req: RequestWithFiles, res, next) => {
+  if (!req.headers["content-type"]?.startsWith("multipart/form-data;"))
     return res.status(422).json({ error: "Only accepts form-data!" });
 
-  const form = formidable({multiples:false})
+  const form = formidable({ multiples: false });
 
-  form.parse(req,(err,fields,files) => {
+  form.parse(req, (err, fields, files) => {
+    if (err) return next(err);
 
-    if(err) return next(err)
-    req.body = fields 
-    req.files = files
+    req.body = fields;
+    req.files = files;
 
-    next()
-  })
-}
+    next();
+  });
+};
 
-export default fileParser
+export default fileParser;
