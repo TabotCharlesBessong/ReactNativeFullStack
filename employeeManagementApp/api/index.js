d887f1eae0c23e2f8889601e8f7f2df3ac21fdf2
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-12-24 16:07:25 +0100

    creating employees record

--- employeeManagementApp/api/index.js
+++ employeeManagementApp/api/index.js
@@ -1,26 +1,65 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const moment = require("moment");
+const dotenv = require("dotenv");
+// const { addEmployee } = require('./controller/employee.controller.js')
+const Employee = require("./models/employee.js");
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const moment = require('moment')
-const dotenv = require('dotenv')
-
-const app = express()
-const port = 5000 || process.env.PORT
-app.use(cors())
-dotenv.config()
+const app = express();
+const port = 5000 || process.env.PORT;
+app.use(cors());
+dotenv.config();
 
 // middlewares
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  console.log("Connected to the DB!!!")
-}).catch((error) => {
-  console.log("Error connecting to MongoDB ",error)
-})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to the DB!!!");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB ", error);
+  });
 
 app.listen(port, () => {
-  console.log(`The server is running on port number ${port}.....`)
-})
+  console.log(`The server is running on port number ${port}.....`);
+});
+
+app.post("/add-employee", async (req, res) => {
+  try {
+    const {
+      employeeName,
+      employeeId,
+      designation,
+      phoneNumber,
+      dateOfBirth,
+      joiningDate,
+      activeEmployee,
+      salary,
+      address,
+    } = req.body;
+    const newEmployee = new Employee({
+      employeeName,
+      employeeId,
+      designation,
+      phoneNumber,
+      dateOfBirth,
+      joiningDate,
+      activeEmployee,
+      salary,
+      address,
+    });
+
+    await newEmployee.save();
+    res
+      .status(201)
+      .json({ message: "Employee saved successfully", employee: newEmployee });
+  } catch (error) {
+    console.log("Error creating an employee", error);
+    res.status(500).json({ message: "Failed to add employee" });
+  }
+});
