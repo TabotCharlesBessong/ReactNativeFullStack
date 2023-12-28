04060becf07087cdeec615236772da59005276a1
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-12-28 16:17:46 +0100

    employees screen

--- employeeManagementApp/app/(home)/employees.js
+++ employeeManagementApp/app/(home)/employees.js
@@ -1,14 +1,81 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "expo-router";
+import axios from "axios";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
 
 const employees = () => {
+  const [employees, setEmployees] = useState([]);
+  const [input, setInput] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchEmployeeData = async () => {
+      try {
+        const response = await axios.get("http:192.168.207.29:5000/employees");
+        // const response = await axios.get("http:192.168.1.185:5000/employees");
+        setEmployees(response.data);
+      } catch (error) {
+        console.log("Error fetching employee data: ", error);
+      }
+    };
+    fetchEmployeeData();
+  }, []);
+  console.log(employees);
   return (
-    <View>
-      <Text>employees</Text>
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          backgroundColor: "white",
+        }}
+      >
+        <Ionicons
+          name="arrow-back"
+          size={24}
+          color="black"
+          onPress={() => router.back()}
+          style={{ marginLeft: 10 }}
+        />
+        <Pressable
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginHorizontal: 7,
+            gap: 10,
+            backgroundColor: "white",
+            borderRadius: 3,
+            height: 40,
+            flex: 1,
+          }}
+        >
+          <AntDesign
+            style={{ marginLeft: 10 }}
+            name="search1"
+            size={20}
+            color="black"
+          />
+          <TextInput
+            value={input}
+            onChangeText={(text) => setInput(text)}
+            style={{ flex: 1 }}
+            placeholder="Search"
+          />
+
+          {employees.length > 0 && (
+            <View>
+              <Pressable onPress={() => router.push("/(home)/adddetails")}>
+                <AntDesign name="pluscircle" size={30} color="#0072b1" />
+              </Pressable>
+            </View>
+          )}
+        </Pressable>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default employees
+export default employees;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
