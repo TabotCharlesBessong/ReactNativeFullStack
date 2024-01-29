1be092b56225535a378c3450309dc0143c2ccb36
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-01-29 22:16:55 +0100

    starting a linked clone

---
+++ linkedIn_Clone/api/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const moment = require("moment");
+const dotenv = require("dotenv");
+
+const app = express();
+const port = 5000 || process.env.PORT;
+app.use(cors());
+dotenv.config();
+
+// middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to the DB!!!");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB ", error);
+  });
+
+app.listen(port, () => {
+  console.log(`The server is running on port number ${port}.....`);
+});
---
+++ linkedIn_Clone/app/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text } from 'react-native'
+
+const index = () => {
+  return (
+    <Text>
+      Hello LinkedIn
+    </Text>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
