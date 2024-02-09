cf9227173336f6f036126e0d9c279b6ecc90dd97
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-02-09 16:16:24 +0100

    connections

--- linkedIn_Clone/app/(tabs)/network/connections.js
+++ linkedIn_Clone/app/(tabs)/network/connections.js
@@ -1,14 +1,115 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import { AntDesign, Entypo, Feather, Octicons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
 const connections = () => {
+  const [connections, setConnections] = useState([]);
+
+  const [userId, setUserId] = useState("");
+  useEffect(() => {
+    const fetchUser = async () => {
+      const token = await AsyncStorage.getItem("authToken");
+      const decodedToken = jwtDecode(token);
+      const userId = decodedToken.userId;
+      setUserId(userId);
+    };
+
+    fetchUser();
+  }, []);
+
+  useEffect(() => {
+    if (userId) {
+      fetchConnections();
+    }
+  }, [userId]);
+
+  const fetchConnections = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/connection/${userId}`
+      );
+      setConnections(response.data.connections);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(connections);
   return (
-    <View>
-      <Text>connections</Text>
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginHorizontal: 12,
+          marginTop: 10,
+        }}
+      >
+        <Text style={{ fontWeight: "500" }}>
+          {connections?.length} Connections
+        </Text>
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+          <AntDesign name="search1" size={22} color="black" />
+          <Octicons name="three-bars" size={22} color="black" />
+        </View>
+      </View>
+
+      <View
+        style={{
+          height: 2,
+          borderColor: "#E0E0E0",
+          borderWidth: 2,
+          marginTop: 12,
+        }}
+      />
+
+      <View style={{ marginHorizontal: 10, marginTop: 10 }}>
+        {connections?.map((item, index) => (
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+              marginVertical: 10,
+            }}
+            key={index}
+          >
+            <Image
+              style={{ width: 48, height: 48, borderRadius: 24 }}
+              source={{ uri: item?.profileImage }}
+            />
+
+            <View style={{ flexDirection: "column", gap: 2, flex: 1 }}>
+              <Text style={{ fontSize: 15, fontWeight: "500" }}>
+                {item?.name}
+              </Text>
+
+              <Text style={{ color: "gray" }}>
+                B.Tech | Computer Science Technology
+              </Text>
+
+              <Text style={{ color: "gray" }}>
+                connected on {moment(item?.createdAt).format("MMMM Do YYYY")}
+              </Text>
+            </View>
+
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 8 }}
+            >
+              <Entypo name="dots-three-vertical" size={20} color="black" />
+              <Feather name="send" size={20} color="black" />
+            </View>
+          </View>
+        ))}
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default connections
+export default connections;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
