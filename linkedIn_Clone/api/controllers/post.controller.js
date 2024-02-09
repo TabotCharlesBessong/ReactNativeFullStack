28151253df3a1b430b677fab7e3334291c861788
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-02-09 14:26:03 +0100

    like and unliking post

--- linkedIn_Clone/api/controllers/post.controller.js
+++ linkedIn_Clone/api/controllers/post.controller.js
@@ -30,4 +30,31 @@ const getAllPosts = async (req, res) => {
   }
 };
 
-module.exports = { create, getAllPosts };
+const likePost = async (req,res) => {
+  try {
+    const postId = req.params.postId
+    const userId = req.params.userId
+
+    const post = await Post.findByid(postId)
+    if(!post){
+      return res.status(400).json({message:"Post not found!"})
+    }
+
+    // check if the user has already liked the post
+    const existingLike = post?.likes.find((like) => like.user.toString() === userId)
+
+    if(existingLike){
+      post.likes = post.likes.filter((like) => like.user.toString() !== userId)
+    }else{
+      post.likes.push({user:userId})
+    }
+
+    await post.save()
+    res.status(200).json({message:"You successfully liked the post"})
+  } catch (error) {
+    console.log("Error like a post:", error)
+    res.status(500).json({message:"Error liking the post"})
+  }
+}
+
+module.exports = { create, getAllPosts,likePost };
