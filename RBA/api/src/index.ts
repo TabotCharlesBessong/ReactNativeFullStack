6be0ee488a7da5de79302e5593260a5bc5ee2178
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-05-05 17:01:47 +0100

    created an express server

--- RBA/api/src/index.ts
+++ RBA/api/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world")
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+const port = (process.env.PORT || 5000) as string
+
+app.listen(port,() => {
+  console.log(`App is running on http://localhost:${port}`)
+})
