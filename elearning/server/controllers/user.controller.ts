556153ccdbde4727588d73003feacfcf8d29cadd
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-07-20 05:25:31 +0100

    activating user

--- elearning/server/controllers/user.controller.ts
+++ elearning/server/controllers/user.controller.ts
@@ -2,7 +2,7 @@ import ejs from "ejs";
 import jwt, { Secret } from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
-import userModel from "../models/user.model";
+import userModel, { IUser } from "../models/user.model";
 import ErrorHandler from "../utils/ErrorHandler";
 import path from "path";
 import sendMail from "../utils/sendMail";
@@ -76,3 +76,46 @@ export const createActivationToken = (user: any): IActivationToken => {
 
   return { token, activationCode };
 };
+
+// activate user
+interface IActivationRequest {
+  activation_token: string;
+  activation_code: string;
+}
+
+export const activateUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { activation_token, activation_code } =
+        req.body as IActivationRequest;
+
+      const newUser: { user: IUser; activationCode: string } = jwt.verify(
+        activation_token,
+        process.env.JWT_SECRET as string
+      ) as { user: IUser; activationCode: string };
+
+      if (newUser.activationCode !== activation_code) {
+        return next(new ErrorHandler("Invalid activation code", 400));
+      }
+
+      const { name, email, password } = newUser.user;
+
+      const existUser = await userModel.findOne({ email });
+
+      if (existUser) {
+        return next(new ErrorHandler("Email already exist", 400));
+      }
+      const user = await userModel.create({
+        name,
+        email,
+        password,
+      });
+
+      res.status(201).json({
+        success: true,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
--- elearning/server/routes/user.router.ts
+++ elearning/server/routes/user.router.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import { registrationUser } from '../controllers/user.controller';
+import { activateUser, registrationUser } from '../controllers/user.controller';
 const userRouter = express.Router()
 
 userRouter.post("/registration", registrationUser);
+userRouter.post("/activate", activateUser);
 
 export default userRouter
