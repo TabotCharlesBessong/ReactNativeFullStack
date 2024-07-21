52f5c4e11f6e48362e57c1a849862e0d6fd996af
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-07-21 22:38:58 +0100

    social auth

--- elearning/server/controllers/user.controller.ts
+++ elearning/server/controllers/user.controller.ts
@@ -203,3 +203,39 @@ export const updateAccessToken = CatchAsyncError(
     
   }
 )
+
+// get user info
+export const getUserInfo = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId = req.user?._id;
+      getUserById(userId, res);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+interface ISocialAuthBody {
+  email: string;
+  name: string;
+  avatar: string;
+}
+
+// social auth
+export const socialAuth = CatchAsyncError(
+  async (req:Request,res:Response,next:NextFunction) => {
+    try{
+      const {email,name,avatar} = req.body as ISocialAuthBody
+      const user = await userModel.findOne({email})
+      if(!user){
+        const newUser = await userModel.create({email,name,avatar})
+        sendToken(newUser,200,res)
+      }else{
+        sendToken(user,200,res)
+      }
+    }catch(error:any){
+      return next(new ErrorHandler(error.message,400))
+    }
+  }
+)
